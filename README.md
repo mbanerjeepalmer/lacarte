554a2d9569a4607151a18f6ddc2a267ec5c42f2d
Maurice <mbanerjeepalmer@gmail.com>
2025-01-08 14:45:23 +0200

    here we go svelte hack speedrun

---
+++ README.md
@@ -0,0 +1,5 @@
+# Lacarte
+Explore media.
+
+## Licence
+I haven't decided how to license this yet. So it remains under copyright.
---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	// Mock data - just numbers for now
+	const items = Array.from({ length: 25 }, (_, i) => i + 1);
+</script>
+
+<div class="h-screen w-screen snap-both snap-mandatory overflow-auto">
+	<div class="grid grid-cols-[repeat(5,90vw)] grid-rows-[repeat(5,90vh)] gap-4 p-4">
+		{#each items as item}
+			<div
+				class="flex min-h-[90vh] min-w-[90vw] snap-center items-center
+					   justify-center rounded-xl bg-white text-4xl shadow-lg"
+			>
+				{item}
+			</div>
+		{/each}
+	</div>
+</div>
